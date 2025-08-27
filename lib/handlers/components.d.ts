3411ca8545f211bbb1845946d867f3ffb2539e8f
kgsi <kuusai1998@gmail.com>
2025-08-27 18:36:54 +0900

    ignore修正

--- lib/handlers/components.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ComponentInfo, ComponentDetail, DiscoveryOptions } from '../types/index.js';
-export declare class ComponentHandlers {
-    listComponents(options?: DiscoveryOptions): Promise<ComponentInfo[]>;
-    getComponent(name: string): Promise<ComponentDetail | null>;
-    searchComponents(query: string): Promise<ComponentInfo[]>;
-    getComponentsByCategory(category: string): Promise<ComponentInfo[]>;
-    generateComponentCode(componentName: string, props: Record<string, any>): Promise<string>;
-}
-//# sourceMappingURL=components.d.ts.map
--- lib/utils/componentDiscovery.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ComponentInfo, ComponentDetail, DiscoveryOptions } from '../types/index.js';
-export declare function discoverComponents(options?: DiscoveryOptions): Promise<ComponentInfo[]>;
-export declare function getComponentDetails(componentName: string): Promise<ComponentDetail | null>;
-//# sourceMappingURL=componentDiscovery.d.ts.map
